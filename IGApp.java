59481ffe7b303f534212ea5d20f8996f9f556c3e
yeeechenggg <yeecheng.koh@gmail.com>
2023-12-14 14:41:38 +0800

    starting

--- IGApp.java
+++
@@ -1,8 +0,0 @@
-public class IGApp {
-    public static void main(String[] args) {
-        SearchCtrl ctrl = new SearchCtrl();
-        SearchMenu menu = new SearchMenu(ctrl);
-        menu.readOption();
-    }
-}
-
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project-Heartcode-group-16
 use of HTML, CSS,and Bootstrap to code a simple site
+
+
